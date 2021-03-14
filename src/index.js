3b1f85a9231ce5b67021524619ce20f8cb79df28
Kostya Vyazovyk <vyazovyk@gmail.com>
2021-03-14 15:25:40 +0200

    feat: add typical-arrays-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  return array.reduce((total, amount) => total + amount) / array.length;
 }
